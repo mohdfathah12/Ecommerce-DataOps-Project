61cd856173ec05878c748edd7e63d6621101286b
Muhammed Fathah <mohdfathah12@gmail.com>
2026-03-04 18:04:57 +0530

    Added Data Quality checks for Price and Quantity

--- Dataopes/src/silver_cleaning.py
+++ Dataopes/src/silver_cleaning.py
@@ -6,7 +6,14 @@ def clean_data(df):
     .withColumn("timestamp", F.to_timestamp("timestamp")) \
     .withColumn("price", F.col("price").cast("double")) \
     .withColumn("status", F.lower(F.col("status")))
-    return cleaned_df
+
+    final_df = cleaned_df \
+    .filter(F.col("price") > 0) \
+    .filter(F.col("quantity") > 0)
+    print (f"Silver Layer Cleaning: Filtered out bad records. Final row count:{final_df.count()}")
+    return final_df
+
+
 
 
 

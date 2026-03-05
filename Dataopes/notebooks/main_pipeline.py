af6986ee27144cf8fb6d9464acb026d5397550eb
Muhammed Fathah <mohdfathah12@gmail.com>
2026-03-06 00:29:26 +0530

    Complete E2E pipeline logic added

--- Dataopes/notebooks/main_pipeline.py
+++ Dataopes/notebooks/main_pipeline.py
@@ -1,19 +1,53 @@
 import sys
 import os
-sys.path.append(os.path.abspath('/Workspace/Repos/mohdfathah12@gmail.com/Ecommerce-DataOps-Project/src'))
+from pyspark.sql import SparkSession
 
-from bronze_ingestion import ingest_bronze
-from silver_cleaning import clean_data
-from gold_analytics import create_gold_report
+current_dir = os.getcwd()
+parent_dir = os.path.dirname(current_dir)
+src_path = os.path.join(parent_dir, "src")
 
-bronze_df = ingest_bronze(spark)
+if src_path not in sys.path:
+    sys.path.append(src_path)
+    print(f"Added to path: {src_path}")
 
-silver_df = clean_data(bronze_df)
-silver_df.write.format("delta").mode("overwrite").saveAsTable("fmcg.default.ecommerce_silver")
+try:
+    from bronze_ingestion import ingest_bronze
+    from silver_cleaning import clean_data
+    from gold_analytics import create_gold_report
 
-gold_df = create_gold_report(silver_df)
-gold_df.write.format("delta").mode("overwrite").saveAsTable("fmcg.default.ecommerce_gold")
+    print("Success: Modules imported!")
+except ModuleNotFoundError:
+    root_dir = os.path.dirname(parent_dir)
+    src_path_alt = os.path.join(root_dir, "src")
+    
+    sys.path.append(src_path_alt)
 
-print("End-to-End DataOps Pipeline Successful!")
-display(spark.table("fmcg.default.ecommerce_gold"))
+    from bronze_ingestion import ingest_bronze
+    from silver_cleaning import clean_data
+    from gold_analytics import create_gold_report
 
+    print(f"Success: Modules imported from alternative path: {src_path_alt}")
+
+spark = SparkSession.builder.appName("MedallionPipeline").getOrCreate()
+
+
+try:
+
+    print("Starting Bronze Ingestion...")
+
+    df_bronze = ingest_bronze(spark)
+
+
+    print("Starting Silver Cleaning...")
+    df_silver = clean_data(df_bronze)
+    df_silver.write.format("delta").mode("overwrite").saveAsTable("silver_orders")
+
+
+    print("Starting Gold Analytics...")
+    df_gold = create_gold_report(df_silver)
+    df_gold.write.format("delta").mode("overwrite").saveAsTable("gold_order_metrics")
+
+    print(" SUCCESS: All layers (Bronze, Silver, Gold) processed!")
+
+except Exception as e:
+    print(f" ERROR: Pipeline failed due to: {e}")
